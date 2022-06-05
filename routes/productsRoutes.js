13612a7e285486fec648ee931e07682888c15f0d
danieldlf <105021676+danieldlf@users.noreply.github.com>
2022-06-05 04:41:19 -0300

    Update productsRoutes.js

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -5,5 +5,10 @@ const ProductController = require('../controllers/ProductController')
 const checkAuth = require('../helpers/auth').checkAuth
 
 router.get('/dashboard', checkAuth, ProductController.dashboard)
+router.get('/criar', checkAuth, ProductController.criarReq)
+router.post('/criar', checkAuth, ProductController.criarReqSave)
+router.post('/deletar', checkAuth, ProductController.deletarProd)
+router.get('/redefinirProd/:id', checkAuth, ProductController.redefinirProd)
+router.post('/redefinirProd', checkAuth, ProductController.redefinirProdPost)
 
 module.exports = router
