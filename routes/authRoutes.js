fa93f83d6f788758ee17eed7f9f63f9f0185e81d
danieldlf <105021676+danieldlf@users.noreply.github.com>
2022-06-04 21:42:01 -0300

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,5 +4,11 @@ const AuthController = require('../controllers/AuthController')
 
 router.get('/cadastro', AuthController.cadastro)
 router.post('/cadastro', AuthController.cadastroPost)
+router.get('/login', AuthController.login)
+router.post('/login', AuthController.loginPost)
+router.get('/redefinir', AuthController.redefinir)
+router.post('/redefinir', AuthController.redefinirPost)
+router.get('/', AuthController.home)
+router.get('/logout', AuthController.logout)
 
 module.exports = router
