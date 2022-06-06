d06c8034d5f8bbafde87db05a109bef17b36c63c
danieldlf <105021676+danieldlf@users.noreply.github.com>
2022-06-06 05:06:29 -0300

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -5,6 +5,7 @@ const db = require('../db/conn')
 // Ligação com o User
 const User = require('./User')
 
+// pname = nome do produto, pqty = quantidade, reqst = boolean pra conseguir diferenciar requisições dos agronomos de disponibilidades do agricultor
 const Product = db.define('Product', {
     pname: {
         type: DataTypes.STRING,
@@ -13,6 +14,11 @@ const Product = db.define('Product', {
     },
     pqty: {
         type: DataTypes.STRING,
+        allowNull: false,
+        required: true
+    },
+    reqst: {
+        type: DataTypes.BOOLEAN,
         allowNull: false,
         required: true
     }
